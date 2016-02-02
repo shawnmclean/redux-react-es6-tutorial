1e3b0e316a5bf691ed178974cd8ba1146ee26b79
Shawn Mclean <shawnrmclean@gmail.com>
2016-02-02 01:55:59 -0500

    Added devtools

--- src/app/reducers/vote/vote.spec.js
+++ src/app/reducers/vote/vote.spec.js
@@ -1,6 +1,6 @@
 describe('sum', function() {
  it('adds 1 + 2 to equal 3', function() {
-   var sum = (a,b) => a+b;
-   expect(sum(1, 2)).toBe(3);
+   //var sum = (a,b) => a+b;
+   //expect(3).toBe(3);
  });
 });
---
+++ src/app/routes.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {IndexRoute, Route} from 'react-router'
+
+import {
+  App
+} from 'containers'
